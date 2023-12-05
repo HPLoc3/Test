14e0f22252e4013eb2770b59aacbc8e21f8db86b
HPLoc3 <phuloctkxb@gmail.com>
2023-12-05 14:52:38 +0700

    Test

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $group.artifact1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
